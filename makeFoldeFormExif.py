6245697b84bcb5e04204f4ec5716c2eb097ab201
Tiago S. Fraga <thyaggus@gmail.com>
2018-02-25 23:56:55 -0300

    Updating project with files

---
+++ makeFoldeFormExif.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import sys
+import os
+import exifread
+from shutil     import copyfile
+from os         import listdir
+from os.path    import isfile, join
+
+
+root = "";
+
+def moveFile (dir, file):
+    directory = "./sorted/" + dir
+    destiny = directory + "/" + file
+        
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    copyfile(file, destiny)
+    
+
+
+def extracData ():
+    fileList = [fl for fl in listdir(".") if isfile(join(".", fl))]
+    for file in fileList:
+        if file.endswith(".jpeg") or file.endswith(".jpg") or file.endswith(".tiff") or file.endswith(".JPEG") or file.endswith(".JPG") or file.endswith(".TIFF"):
+            tags = exifread.process_file(open(file, 'rb'))
+            for tag in tags.keys():
+                if tag in ('EXIF DateTimeOriginal'):
+                    folder = str(tags[tag])
+                    folder = folder[:10].replace(":", "-")
+                    moveFile(folder, file)
+
+def main():
+    extracData()
+
+if __name__ == "__main__":
+    main()
