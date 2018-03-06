679b9c7d0c33ed3b4c6f72c1351e1366ab23264d
Tiago <thyaggus@gmail.com>
2018-03-06 10:40:29 -0300

    progresse bar
    Start to implements a progress bar.

--- makeFoldeFormExif.py
+++ makeFoldeFormExif.py
@@ -36,3 +36,44 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
+    
+    #### begin of experiment
+
+def display(i, total):
+	i*100/total
+	processed = int(percent*20/100)
+	progress = ""
+	blank = ""
+
+	for p in range (i):
+		progress = progress + "="
+	for b in range (total - i):
+		blank = blank + " "
+	progress = progress + blank
+
+    if i % 4 == 1:
+    	print ("| [%s]", progress, end="")
+	elif i % 4 == 2:
+		print ("/ [%s]", progress, end="")
+	elif i % 4 == 3:
+		print ("- [%s]", progress, end="")
+	else:
+		print ("\\ [%s]", progress, end="")
+
+def extracData ():
+    fileList = [fl for fl in listdir(".") if isfile(join(".", fl))]
+    totalFiles = len(fileList)
+    for i range (totalFiles)
+    	file = fileList[i]
+        if file.endswith(".jpeg") or file.endswith(".jpg") or file.endswith(".tiff") or file.endswith(".JPEG") or file.endswith(".JPG") or file.endswith(".TIFF"):
+            tags = exifread.process_file(open(file, 'rb'))
+            for tag in tags.keys():
+                if tag in ('EXIF DateTimeOriginal'):
+                    folder = str(tags[tag])
+                    folder = folder[:10].replace(":", "-")
+                    moveFile(folder, file)
+                    display(i, totalFiles)
+
+
+#### end of experiment
