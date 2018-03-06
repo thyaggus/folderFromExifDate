8176b1accd23de43e47701e1160aa84c2e94f457
Tiago <thyaggus@gmail.com>
2018-03-06 11:24:15 -0300

    Add marks
    Add mark to indicate when process starts and finishs.

--- makeFoldeFormExif.py
+++ makeFoldeFormExif.py
@@ -21,6 +21,7 @@ def moveFile (dir, file):
 
 
 def extracData ():
+    print("Start processing ...")
     fileList = [fl for fl in listdir(".") if isfile(join(".", fl))]
     for file in fileList:
         if file.endswith(".jpeg") or file.endswith(".jpg") or file.endswith(".tiff") or file.endswith(".JPEG") or file.endswith(".JPG") or file.endswith(".TIFF"):
@@ -30,50 +31,10 @@ def extracData ():
                     folder = str(tags[tag])
                     folder = folder[:10].replace(":", "-")
                     moveFile(folder, file)
+     print("Finished")
 
 def main():
     extracData()
 
 if __name__ == "__main__":
     main()
-    
-    
-    #### begin of experiment
-
-def display(i, total):
-	percent = i*100/total
-	processed = int(percent*70/100)
-	progress = ""
-	blank = ""
-
-	for p in range (i):
-		progress = progress + "="
-	for b in range (total - i):
-		blank = blank + " "
-	progress = progress + blank
-
-    if i % 4 == 1:
-    	print ("| [%s]", progress, end="")
-	elif i % 4 == 2:
-		print ("/ [%s]", progress, end="")
-	elif i % 4 == 3:
-		print ("- [%s]", progress, end="")
-	else:
-		print ("\\ [%s]", progress, end="")
-
-def extracData ():
-    fileList = [fl for fl in listdir(".") if isfile(join(".", fl))]
-    totalFiles = len(fileList)
-    for i in range (totalFiles)
-    	file = fileList[i]
-        if file.endswith(".jpeg") or file.endswith(".jpg") or file.endswith(".tiff") or file.endswith(".JPEG") or file.endswith(".JPG") or file.endswith(".TIFF"):
-            tags = exifread.process_file(open(file, 'rb'))
-            for tag in tags.keys():
-                if tag in ('EXIF DateTimeOriginal'):
-                    folder = str(tags[tag])
-                    folder = folder[:10].replace(":", "-")
-                    moveFile(folder, file)
-                    display(i, totalFiles)
-
-
-#### end of experiment
